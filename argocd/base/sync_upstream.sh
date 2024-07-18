a236eda2a71dcafd11918c49de10c916bd677542
danielkubat <dan.kubat@gmail.com>
2024-07-18 13:06:19 +0200

    Bump version to v2.11.5

--- argocd/base/sync_upstream.sh
+++ argocd/base/sync_upstream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the version and URL of the file to download
-VERSION=2.11.4 # https://github.com/argoproj/argo-cd/releases/tag/v2.11.4
+VERSION=2.11.5 # https://github.com/argoproj/argo-cd/releases/tag/v2.11.5
 URL="https://raw.githubusercontent.com/argoproj/argo-cd/v${VERSION}/manifests/install.yaml"
 
 # Define the local path of the file to be replaced
